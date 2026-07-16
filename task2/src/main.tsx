a1c03530bc0e52315149805281dfebc216e36e32
mort-the-adventurer <13525048@std.stei.itb.ac.id>
2026-07-16 23:21:59 +0700

    Initialize React app with Tailwind CSS
    Set up React application with Tailwind CSS integration.

--- task2/src/main.tsx
+++ task2/src/main.tsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css' // Mengimpor Tailwind yang sudah disetup di atas
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
